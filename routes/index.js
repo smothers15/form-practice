272a5a0802749fb21b7c388b4771fc641a54a8ac
smothers15 <dylo@fresnou.org>
2021-10-25 08:57:33 -0700

    form practice

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/results', function(req, res, next) {
+
+  console.log(req.body.colors);
+  console.log("underlined" + req.body.underlined);
+  res.render('result',{
+    color: req.body.colors,
+    text: req.body.textInput,
+    underlined: req.body.underlined
+  });
 });
 
 module.exports = router;
