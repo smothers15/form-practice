8076f47bf9480325ebcf6645b147c4ce3ed6da7a
smothers15 <dylo@fresnou.org>
2021-11-03 08:58:02 -0700

    form practice border and text color

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,33 @@ router.get('/', function(req, res, next) {
 
 router.post('/results', function(req, res, next) {
 
-
+  //console.log("Width: " + borderWidth + " Color: " + borderColor);
   //console.log(size);
   res.render('result',{
     color: req.body.colors,
     text: req.body.textInput||"This is an example of something you could type here.",
     underlined: req.body.underlined,
     size: req.body.size,
+    borderWidth: req.body.borderWidth,
+    borderColor: req.body.border,
+    textColor: req.body.textColor
 
   });
+
 });
+
+/*function getBorderColor(border){
+  if(border === "black"){
+    return "#000000"
+  }else if(type === "blue"){
+    return "#0000FF"
+  }else if(type === "red"){
+    return "#FF0000"
+  }else if(type === "purple"){
+    return "#800080"
+  }else{
+    return "#008000"
+  }
+}*/
 
 module.exports = router;
